16469ce0236d78de02e49d5bd28d1287869d6025
charles_stark <1614869409@qq.com>
2022-12-14 23:39:05 +0800

    Bug Fixed

---
+++ backend/src/main/java/com/example/backend/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.backend.Service.Impl;
+
+import com.example.backend.Service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
