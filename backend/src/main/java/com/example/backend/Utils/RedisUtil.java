5d7fb54b30b5b3b4eb4488d0077f677cc8bef384
charles_stark <1614869409@qq.com>
2022-12-23 19:50:31 +0800

    Add Redis

---
+++ backend/src/main/java/com/example/backend/Utils/RedisUtil.java
@@ -0,0 +1,59 @@
+package com.example.backend.Utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisUtil {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    public String get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public boolean set(String key, String value) {
+        try {
+            redisTemplate.opsForValue().set(key, value);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean setWithTimeout(String key, String value, long timeout, TimeUnit unit) {
+        try {
+            redisTemplate.opsForValue().set(key, value, timeout, unit);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean getAndSet(String key, String value) {
+        try {
+            redisTemplate.opsForValue().getAndSet(key, value);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean delete(String key) {
+        try {
+            redisTemplate.delete(key);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
