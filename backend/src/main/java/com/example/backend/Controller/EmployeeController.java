2925830462294f8126380d5c0127e7087dcad398
charles_stark <1614869409@qq.com>
2022-12-29 13:36:37 +0800

    Update

--- backend/src/main/java/com/example/backend/Controller/EmployeeController.java
+++ backend/src/main/java/com/example/backend/Controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.example.backend.Controller;
 
+import com.example.backend.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/employee")
 public class EmployeeController {
 
-
+    @Autowired
+    EmployeeService employeeService;
 
 }
