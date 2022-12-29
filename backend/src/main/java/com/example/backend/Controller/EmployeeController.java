dabe6922b6d9994eb9c6120e32d66276c477efc7
charles_stark <1614869409@qq.com>
2022-12-29 13:31:21 +0800

    Add EmployeeController

---
+++ backend/src/main/java/com/example/backend/Controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.example.backend.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+
+
+}
