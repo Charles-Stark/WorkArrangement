4396ad0936a26ef3c8cba008670ab84c61534b7b
charles_stark <1614869409@qq.com>
2022-12-29 13:34:54 +0800

    Add EmployeeService

---
+++ backend/src/main/java/com/example/backend/Service/Impl/EmployeeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.backend.Service.Impl;
+
+import com.example.backend.Service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+
+}
