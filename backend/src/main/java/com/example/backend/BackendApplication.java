3de0bf8be9cfdb6cbf575bbc5535a5f8941e4ea2
charles_stark <1614869409@qq.com>
2023-01-04 15:27:57 +0800

    Update TimeZone

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -4,11 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @MapperScan(value = "com.example.backend.mapper")
 public class BackendApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(BackendApplication.class, args);
     }
 
