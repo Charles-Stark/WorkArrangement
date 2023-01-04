26bc28e9ef3b6d3ba32b599d544f63ccf654d620
charles_stark <1614869409@qq.com>
2023-01-04 14:26:39 +0800

    Add CustomerFlow

---
+++ backend/src/main/java/com/example/backend/POJO/CustomerFlow.java
@@ -0,0 +1,35 @@
+package com.example.backend.POJO;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerFlow {
+
+    private Long id;
+    private Long shop;  // 该客流量数据所属门店的id
+    private Date date;  // 该客流量数据的日期
+    @TableField(typeHandler = JacksonTypeHandler.class)
+    private ArrayList<FlowUnit> flowUnits;  // 当天的客流量数据列表
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class FlowUnit {
+        // 客流量数据最小单元，一个FlowUnit代表半个小时
+
+        private Date beginAt;  // 该单元开始的时间
+        private Date endAt;  // 该单元结束的时间
+        private double flow;  // 该单元的客流量大小
+
+    }
+
+}
