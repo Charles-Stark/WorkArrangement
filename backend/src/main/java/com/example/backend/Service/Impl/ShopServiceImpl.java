9e180024a5a88d1e193261e9effbed3b9ee65472
charles_stark <1614869409@qq.com>
2023-01-07 14:13:06 +0800

    Update ShopService

--- backend/src/main/java/com/example/backend/Service/Impl/ShopServiceImpl.java
+++ backend/src/main/java/com/example/backend/Service/Impl/ShopServiceImpl.java
@@ -7,8 +7,6 @@ import com.example.backend.mapper.ShopMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
 public class ShopServiceImpl implements ShopService {
 
