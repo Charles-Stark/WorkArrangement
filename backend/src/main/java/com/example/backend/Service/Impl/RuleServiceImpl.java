8278a891d478b88885db0c6c8ec3ab3ae340afa7
charles_stark <1614869409@qq.com>
2023-01-18 15:27:03 +0800

    Add RuleService

---
+++ backend/src/main/java/com/example/backend/Service/Impl/RuleServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.backend.Service.Impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RuleServiceImpl {
+
+
+
+}
---
+++ backend/src/main/java/com/example/backend/Service/RuleService.java
@@ -0,0 +1,19 @@
+package com.example.backend.Service;
+
+import com.example.backend.VO.ResultVO;
+
+public interface RuleService {
+
+    ResultVO<Object> addRule(Long shop, Double prepareTime, Double prepareWorkloadPerPerson, String preparePosition,
+                             Double maxServiceNumber, String servicePosition, Integer numberOnDuty, Double losingTime,
+                             Double closingWorkloadPerPersonU, Double closingWorkloadPerPersonV, String closingPosition);
+
+    ResultVO<Object> deleteRule(Long id);
+
+    ResultVO<Object> updateRule();
+
+    ResultVO<Object> getRule(Long id);
+
+    ResultVO<Object> getRuleByShop(Long shopId);
+
+}
