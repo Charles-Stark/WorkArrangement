6bd25667dddcf74569cf7723498dcad6712d01f6
charles_stark <1614869409@qq.com>
2023-03-09 17:20:53 +0800

    Update ScheduleService

--- backend/src/main/java/com/example/backend/Service/ScheduleService.java
+++ backend/src/main/java/com/example/backend/Service/ScheduleService.java
@@ -1,7 +1,11 @@
 package com.example.backend.Service;
 
+import com.example.backend.VO.ResultVO;
+
 public interface ScheduleService {
 
+    ResultVO<Object> getScheduleById(long id);
 
+    ResultVO<Object> getScheduleByShop(long id);
 
 }
